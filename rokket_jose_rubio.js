065dcf6668c6e0d8968e20b345a03da4c0d459fd
jarb29 <jarb29@gmail.com>
2020-07-21 08:36:43 -0400

    soluciones

--- rokket_jose_rubio.js
+++ rokket_jose_rubio.js
@@ -1,3 +1,5 @@
+
+// ##############################
 /*
 1. Composite function
 Write a function called "rokket" which produces the following output when called:
@@ -6,11 +8,15 @@ console.log(rokket(4)(2)(2)) // this outputs 16
 console.log(rokket(8)(2)(1)) // this also outputs 16
 This function must work for any three integer numbers.
 */
+// #############################
 
 // R.) No sabia que se podian agregar numeros fuera de la funcion tendre que investigarlo...
 
 
 
+
+
+// #############################
 /* 
 Longest string
 Write a function called "rokket" which receives an array with several strings. It must output the longest (character length) string in the array.
@@ -18,6 +24,7 @@ Example:
 const list = ['best', 'company', 'ever']
 console.log(rokket(list)) // this outputs 'company' 
 */
+// #############################
 
 
 // const rokket = (lista) => {
@@ -31,6 +38,10 @@ console.log(rokket(list)) // this outputs 'company'
 
 // console.log(rokket(list))
 
+
+
+
+// #############################
 /* 
 String repetition
 Write a function called "rokket" which receives a string A and an integer N and returns a new string with A repeated N times.
@@ -38,6 +49,8 @@ Example:
 console.log(rokket('node', 5)) // this outputs 'nodenodenodenodenode'
 console.log(rokket('abc', 2)) // this outputs 'abcabc'
 */
+// #############################
+
 
 
 
@@ -49,6 +62,9 @@ console.log(rokket('abc', 2)) // this outputs 'abcabc'
 // console.log(rokket('alex', 5))
 
 
+
+
+// #############################
 /* 
 4. Only last names
 Write a function called "rokket" which receives a list of names from a contact book. Each name is an object consisting of a first name and last name. Return a list that
@@ -61,6 +77,10 @@ const contacts = [
 ]
 console.log(rokket(contacts)) // this outputs ['Rokket', 'Bond', 'Potter']
 */
+// #############################
+
+
+
 
 // const rokket = (constact) => {
 //     const last_names = [ ...new Set(constact.map((cont) => cont.lastName))];
@@ -76,6 +96,9 @@ console.log(rokket(contacts)) // this outputs ['Rokket', 'Bond', 'Potter']
 // console.log(rokket(contacts))
 
 
+
+
+// #############################
 /* 
 Unique numbers
 Write a function called "rokket" which receives two number arrays of any size. Return a list containing the intersection of the two arrays (all unique numbers in both
@@ -84,6 +107,9 @@ Example:
 console.log(rokket([1, 2, 5], [2, 1, 6])) // this outputs [1, 2, 5, 6]
 console.log(rokket([1, 2, 3], [4, 5, 6])) // this outputs [1, 2, 3, 4, 5, 6]
 */
+// #############################
+
+
 
 // const rokket = (arr_a, arr_b) => {
 //     let new_arr = arr_a.concat(arr_b);
